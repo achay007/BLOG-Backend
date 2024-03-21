6ef278043d372657bfec9ab7564f65610a0c1959
achay007 <akshayachz09@gmail.com>
2024-03-21 21:06:11 +0530

    un install nodemon

--- src/server.js
+++ src/server.js
@@ -1,16 +1,27 @@
 import express from 'express'
+let articlesInfo= [{
+    name:'learn-react',
+    upvotes:0,
+},
+{
+    name:'learn-node',
+    upvotes:0
+}
 
+]
 const app = express()
 app.use(express.json())
 
-app.post('/hello', (req,res) =>{
-    console.log(req.body)
-    res.send(`Hello ${req.body.name}!`)
-})
+app.put('/api/articles/:name/upvote',(req,res)=>{
 
-app.get('/hello/:name',(req,res)=>{
-    const {name} = req.params
-    res.send(`Hello ${name}`)
+    const {name} = req.params;
+    const article = articlesInfo.find(a => a.name === name)
+    if(article){
+        article.upvotes += 1
+        res.send(`The ${name} article has now ${article.upvotes} upvotes`)
+    }else{
+        res.send('The article doesnt doesnt exist')
+    }
 })
 
 
